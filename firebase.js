0287444cc3e006479033736c6c1c0907ee674a9e
Ahmadalhomsi <tarikuka.aa@gmail.com>
2024-05-09 13:53:42 +0300

    git ignore test

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+asdasdasdas
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCz5YNwFKg3mIQas3GXKllpf0Bv7JMvp6o",
